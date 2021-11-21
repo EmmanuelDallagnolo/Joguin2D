e0ebdaf0a11914597c5313bce471783f7184a04b
EmmanuelDallagnolo <emmanueldallagnolo@gmail.com>
2021-11-21 16:11:03 -0300

    Plataformas/Armadilhas moveis/efeitos sonoros

--- Assets/Scripts/WaypointFollower.cs
+++ Assets/Scripts/WaypointFollower.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class WaypointFollower : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   [SerializeField] private GameObject[] waypoints;
+   private int currentWaypointIndex = 0;
+
+   [SerializeField] private float speed = 2;
+
+   private void Update()
+   {
+      if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1)
+      {
+         currentWaypointIndex++;
+         if (currentWaypointIndex >= waypoints.Length)
+         {
+            currentWaypointIndex = 0;
+         }
+      }
+      transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+   }
 }
